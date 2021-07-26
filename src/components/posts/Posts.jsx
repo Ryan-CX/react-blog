9bda3fa96d96c4ff1449b5c75fed3d9e7bf170c9
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-07-26 14:31:16 -0400

    .

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,14 +1,13 @@
-import Post from "../post/Post";
-import "./posts.css";
+import Post from '../post/Post';
+import './posts.css';
 
 export default function Posts() {
-  return (
-    <div className="posts">
-      <Post img="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-      <Post img="https://images.pexels.com/photos/6758029/pexels-photo-6758029.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-      <Post img="https://images.pexels.com/photos/6711867/pexels-photo-6711867.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-      <Post img="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-      <Post img="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-    </div>
-  );
+	return (
+		<div className='posts'>
+			<Post img='https://images.pexels.com/photos/3217363/pexels-photo-3217363.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' />
+			<Post img='https://images.pexels.com/photos/5011944/pexels-photo-5011944.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+			<Post img='https://images.pexels.com/photos/2654902/pexels-photo-2654902.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+			<Post img='https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' />
+		</div>
+	);
 }
