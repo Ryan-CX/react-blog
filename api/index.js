4cd9c997b0cfcca62fab6626bbbe41a5f26e9d9c
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-07-26 21:30:37 -0400

    some updates

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const PORT = 5000 || process.env.PORT;
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+app.use(express.json()); //used to be body parser
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/auth');
+mongoose
+	.connect(process.env.MONGO, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(console.log('connected to mongoDB'))
+	.catch((e) => {
+		console.log(e);
+	});
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+
+app.listen(PORT, () => {
+	console.log('backend is running');
+});
