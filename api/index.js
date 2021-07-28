63375f29841c50ab5483ba2d69c73ebbeac88d30
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-07-27 21:51:50 -0400

    finished backend
    finished backend

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const PORT = 5000 || process.env.PORT;
 const express = require('express');
 const app = express();
-
+const multer = require('multer');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 dotenv.config();
@@ -9,6 +9,8 @@ app.use(express.json()); //used to be body parser
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+
 mongoose
 	.connect(process.env.MONGO, {
 		useNewUrlParser: true,
@@ -20,9 +22,25 @@ mongoose
 		console.log(e);
 	});
 
+//multer
+const myStorage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, 'images');
+	},
+	filename: (req, file, callback) => {
+		callback(null, req.body.name);
+	},
+});
+
+const upload = multer({ storage: myStorage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+	res.status(200).json('file has been uploaded.');
+});
+
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
 
 app.listen(PORT, () => {
 	console.log('backend is running');
